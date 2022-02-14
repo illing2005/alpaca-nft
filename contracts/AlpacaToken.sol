fef0d32cfddc0b431a338b23d549524da18e115b
Sebastian Illing <sebastian@alpaca.casa>
2022-02-14 12:59:04 +0100

    Add royalties and opensea contract metadata

--- contracts/AlpacaToken.sol
+++ contracts/AlpacaToken.sol
@@ -3,22 +3,40 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "./Royalties.sol";
 
-contract AlpacaToken is Ownable, Pausable, ERC1155Supply {
+contract AlpacaToken is
+    Ownable,
+    Pausable,
+    ERC1155Supply,
+    ERC2981ContractWideRoyalties
+{
+    uint24 private constant ROYALTIES = 500;
 
     string public name;
     string public symbol;
 
     constructor()
         ERC1155(
-            "ipfs://QmVsohKkEpXvGxfMPxZfgZ7bgPUgC3Pdjdzbn4tKZN546g/{id}.json"
+            "ipfs://QmXeRDZUewPN3cep4wGWJkAYYaRexnNciqqu3sCpBMAbir/{id}.json"
         )
     {
         name = "Alpaca NFT";
         symbol = "ALPA";
+        _setRoyalties(msg.sender, ROYALTIES);
         for (uint256 i = 0; i <= 22; i++) {
             _mint(msg.sender, i, 1, "");
         }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC1155, ERC2981Base)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
     function togglePause(bool newPaused) public onlyOwner {
@@ -27,5 +45,10 @@ contract AlpacaToken is Ownable, Pausable, ERC1155Supply {
         } else {
             _unpause();
         }
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return
+            "ipfs://QmXeRDZUewPN3cep4wGWJkAYYaRexnNciqqu3sCpBMAbir/contract_metadata.json";
     }
 }
--- test/AlpacaToken.js
+++ test/AlpacaToken.js
@@ -6,7 +6,6 @@ contract("AlpacaToken", (accounts) => {
   describe("Is Ownable and Resumable", function () {
     beforeEach(async function () {
       this.contract = await AlpacaToken.new({ from: account_one });
-
     });
     it("should have correct owner", async function () {
       const owner = await this.contract.owner.call();
@@ -14,14 +13,24 @@ contract("AlpacaToken", (accounts) => {
     });
 
     it("should be pausable/resumable by owner", async function () {
-      await this.contract.togglePause(true, {from: account_one});
+      await this.contract.togglePause(true, { from: account_one });
 
       const paused = await this.contract.paused.call();
       assert.equal(paused, true, "It should be pausable");
 
-      await this.contract.togglePause(false, {from: account_one});
+      await this.contract.togglePause(false, { from: account_one });
       const unpaused = await this.contract.paused.call();
       assert.equal(unpaused, false, "It should be resumable");
+    });
+
+    it("sets royalties correctly", async function () {
+      const amount = web3.utils.toWei("10", "ether");
+      const royalties = await this.contract.royaltyInfo.call(1, amount);
+      assert.equal(royalties["receiver"], account_one);
+      assert.equal(
+        royalties["royaltyAmount"],
+        web3.utils.toWei("0.5", "ether")
+      );
     });
   });
 });
